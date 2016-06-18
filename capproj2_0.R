4079f68861d8f2cccf70474f46a6e75e5b1fb9d6
GamingF0Sho <daniel@medprocessdesigns.com>
2016-06-18 00:26:01 -0700

    Update capproj2_0.R

--- capproj2_0.R
+++ capproj2_0.R
@@ -1,22 +1,25 @@
+# Importing Order Report File
 Order_Report <- read.csv(file = "C:/Users/GamingFoSho/Documents/wdR/Sales by customer clean.csv", header=TRUE, sep=",", na.strings = "")
 or_df <- data.frame(Order_Report)
 
 
+
+# Importing SKU Sales Report File
 SKU_Sales_Report <- read.csv(file = "C:/Users/GamingFoSho/Documents/wdR/Monthly SKU Sales Report.csv", header=TRUE, sep=",", na.strings = "")
 sku_df <- data.frame(SKU_Sales_Report)
 
-
+library(plyr)
 library(tidyr)
 library(dplyr)
 
-
+# Importing Business Customer File
 Company_Report <- read.csv(file = "C:/Users/GamingFoSho/Documents/wdR/hubspot-crm-companies.csv", header=TRUE, sep=",", na.strings = "")
 c_df <- data.frame(Company_Report)
 
 
+# Eliminating unnecessary variables from c_df
 c_df$Time.Zone <- NULL
 c_df$Lifecycle.Stage <- NULL
-
 c_df$Account.Type <- NULL
 c_df$About.Us <- NULL
 c_df$Budget.Cycle <- NULL
@@ -57,27 +60,48 @@ num_pviews_vec <- ifelse(is.na(num_pviews_vec), 0, num_pviews_vec)
 c_df <- mutate(c_df, Number.of.Pageviews = num_pviews_vec)
 
 
-library(plyr)
+
 summary(or_df)
-or_df <- mutate(or_df, Orders = 1)
-or_df <- ddply(or_df, .(Domain), mutate, Order_Number = cumsum(Orders))
-or_df$Orders <- NULL
-or_df$name <- NULL
 str(or_df)
 str(c_df)
 
 
+
+# Eliminating unnecessary variables from or_df
+
+
 or_num_df1 <- or_df
+
+
+
+or_num_df1$Domain <- tolower(or_num_df1$Domain)
+
+
+
+or_day_vec <- as.Date(or_num_df1$day, "%m/%d/%Y")
+or_num_df1 <- mutate(or_num_df1, day = or_day_vec)
+
+or_num_df1 <- mutate(or_num_df1, Orders = 1)
+or_num_df1 <- or_num_df1[order(or_num_df1$Domain, or_num_df1$day),]
+
+or_num_df1 <- ddply(or_num_df1, .(Domain), mutate, Order_Number = cumsum(Orders))
+or_num_df1$Orders <- NULL
+
+# Creating or_date_df
+or_date_df <- or_num_df1
+or_df2 <- or_date_df
+
+
 or_num_df1$company <- NULL
 or_num_df1$shipping_city <- NULL
 or_num_df1$shipping_province <- NULL
-or_num_df1$day <- NULL
-or_num_df1$year <- NULL
 or_num_df1$email <- NULL
 or_num_df1$traffic_source <- NULL
 or_num_df1$host <- NULL
 or_num_df1$referrer <- NULL
-str(or_num_df1)
+or_num_df1$name <- NULL
+or_num_df1$year <- NULL
+or_num_df1$day <- NULL
 
 
 
@@ -102,15 +126,21 @@ str(or_num_df1)
 or_num_df1 <- spread(or_num_df1, Order_Number, total_sales, fill = 0)
 str(or_num_df1)
 
-
 or_num_df1 <- mutate(or_num_df1, total_revenue = Order_One_Amount + Order_Two_Amount + Order_Three_Amount + Order_Four_Amount + Order_Five_Amount + Order_Six_Amount + Order_Seven_Amount + Order_Eight_Amount + Order_Nine_Amount + Order_Ten_Amount + Order_Eleven_Amount + Order_Twelve_Amount + Order_Thirteen_Amount + Order_Fourteen_Amount)
 str(or_num_df1)
 str(c_df)
-
+head(or_num_df1, n = 30L)
 
 c_df <- mutate(c_df, Domain = Company.Domain.Name)
+c_df$Domain <- tolower(c_df$Domain)
+
+
 c_df$Company.Domain.Name <- NULL
 company_df <- inner_join(or_num_df1, c_df, by = "Domain")
+
+
+
+
 
 
 str(company_df)
@@ -121,7 +151,8 @@ or_df$traffic_source
 
 
 # Adding 1st order traffic source to company_df
-traffic_source_df <- select(or_df, Domain, Order_Number, traffic_source)
+or_df2$Domain <- tolower(or_df2$Domain)
+traffic_source_df <- select(or_df2, Domain, Order_Number, traffic_source)
 traffic_source_df <- filter(traffic_source_df, Order_Number == 1)
 traffic_source_df <- mutate(traffic_source_df, First_Order_Traffic_Source = traffic_source)
 traffic_source_df$Order_Number <- NULL
@@ -240,8 +271,6 @@ company_df$Facebook.Company.Page <- addNAstr(company_df$Facebook.Company.Page, v
 company_df$Website.URL <- addNAstr(company_df$Website.URL, value =  "Unknown")
 company_df$LinkedIn.Company.Page <- addNAstr(company_df$LinkedIn.Company.Page, value =  "Unknown")
 company_df$Twitter.Handle <- addNAstr(company_df$Twitter.Handle, value =  "Unknown")
-company_df$Original.Source.Data.1 <- addNAstr(company_df$Original.Source.Data.1, value =  "Unknown")
-company_df$Original.Source.Type <- addNAstr(company_df$Original.Source.Type, value =  "Unknown")
 company_df$Postal.Code <- addNAstr(company_df$Postal.Code, value =  "Unknown")
 company_df$Year.Founded <- addNAstr(company_df$Year.Founded, value =  "Unknown")
 company_df$Last.Activity.Date <- addNAstr(company_df$Last.Activity.Date, value =  "Unknown")
@@ -265,11 +294,109 @@ na_count <- sapply(company_df, function(y) sum(length(which(is.na(y)))))
 na_count
 str(company_df)
 
+Contact_Report <- read.csv(file = "C:/Users/GamingFoSho/Documents/Contacts Clean.csv", header=TRUE, sep=",", na.strings = "")
+contact_df <- data.frame(Contact_Report)
+
+
+contact_df <- separate(contact_df, Email, c("Email Prefix", "Domain"), sep = "@")
+str(contact_df)
+str(subdf)
+subdf$Number.of.Pageviews
+
+contact_df$Domain <- tolower(contact_df$Domain)
+contact_df <- mutate(contact_df, Count = 1)
+contact_df <- ddply(contact_df, .(Domain), mutate, Domain_Count = cumsum(Count))
+
+contact_df$Domain_Count
+company_df$Associated.Contacts
+
+summarise(company_df, max = max(Associated.Contacts))
+
+contact_df <- filter(contact_df, Domain_Count <= 10)
+
+str(contact_df)
+
+
+
+
+
+
+Contact_Num_vec <- contact_df$Domain_Count
+Contact_Num_vec1 <- ifelse((Contact_Num_vec == 1), c("Contact_One"), c("Unknown"))
+Contact_Num_vec2 <- ifelse((Contact_Num_vec == 2), c("Contact_Two"), Contact_Num_vec1)
+Contact_Num_vec3 <- ifelse((Contact_Num_vec == 3), c("Contact_Three"), Contact_Num_vec2)
+Contact_Num_vec4 <- ifelse((Contact_Num_vec == 4), c("Contact_Four"), Contact_Num_vec3)
+Contact_Num_vec5 <- ifelse((Contact_Num_vec == 5), c("Contact_Five"), Contact_Num_vec4)
+Contact_Num_vec6 <- ifelse((Contact_Num_vec == 6), c("Contact_Six"), Contact_Num_vec5)
+Contact_Num_vec7 <- ifelse((Contact_Num_vec == 7), c("Contact_Seven"), Contact_Num_vec6)
+Contact_Num_vec8 <- ifelse((Contact_Num_vec == 8), c("Contact_Eight"), Contact_Num_vec7)
+Contact_Num_vec9 <- ifelse((Contact_Num_vec == 9), c("Contact_Nine"), Contact_Num_vec8)
+Contact_Num_vec10 <- ifelse((Contact_Num_vec == 10), c("Contact_Ten"), Contact_Num_vec9)
+
+contact_df <- mutate(contact_df, Contact_Count = Contact_Num_vec10)
+str(contact_df)
+
+
+
+
+emails_del_df <- select(contact_df, Domain, Emails.Delivered, Contact_Count)
+head(emails_del_df, n = 300L)
+emails_del_df <- spread(emails_del_df, Contact_Count, Emails.Delivered, fill = 0)
+emails_del_df <- mutate(emails_del_df, Total_Emails_Delivered = Contact_One + Contact_Two + Contact_Three + Contact_Four + Contact_Five + Contact_Six + Contact_Seven + Contact_Eight + Contact_Nine + Contact_Ten)
+emails_del_df <- select(emails_del_df, Domain, Total_Emails_Delivered)
+company_df <- left_join(company_df, emails_del_df, by = "Domain")
+company_df$Emails.Delivered <- NULL
+
+emails_open_df <- select(contact_df, Domain, Emails.Opened, Contact_Count)
+emails_open_df <- spread(emails_open_df, Contact_Count, Emails.Opened, fill = 0)
+emails_open_df <- mutate(emails_open_df, Total_Emails_Opened = Contact_One + Contact_Two + Contact_Three + Contact_Four + Contact_Five + Contact_Six + Contact_Seven + Contact_Eight + Contact_Nine + Contact_Ten)
+emails_open_df <- select(emails_open_df, Domain, Total_Emails_Opened)
+company_df <- left_join(company_df, emails_open_df, by = "Domain")
+
+emails_clicked_df <- select(contact_df, Domain, Emails.Clicked, Contact_Count)
+emails_clicked_df <- spread(emails_clicked_df, Contact_Count, Emails.Clicked, fill = 0)
+emails_clicked_df <- mutate(emails_clicked_df, Total_Emails_Clicked = Contact_One + Contact_Two + Contact_Three + Contact_Four + Contact_Five + Contact_Six + Contact_Seven + Contact_Eight + Contact_Nine + Contact_Ten)
+emails_clicked_df <- select(emails_clicked_df, Domain, Total_Emails_Clicked)
+company_df <- left_join(company_df, emails_clicked_df, by = "Domain")
+
+contact_state_df <- select(contact_df, Domain, State.Region, Contact_Count)
+contact_state_df <- spread(contact_state_df, Contact_Count, State.Region)
+contact_state_df <- select(contact_state_df, Domain, Contact_One)
+contact_state_df$Contact_One <- addNAstr(contact_state_df$Contact_One, value =  "Unknown")
+contact_state_df$Contact_One
+company_df <- left_join(company_df, contact_state_df, by = "Domain")
+
+head(contact_state_df, n = 100L)
+head(company_df$Domain, n = 100L)
+str(contact_state_df)
+
+
+
+
+
+company_df <- mutate(company_df, Emails_Opened_Percent = Total_Emails_Opened / Total_Emails_Delivered)
+company_df$Emails_Opened_Percent <- ifelse(is.na(company_df$Emails_Opened_Percent), 0, company_df$Emails_Opened_Percent)
+company_df$Emails_Opened_Percent
+company_df$Total_Emails_Delivered
+
+company_df <- mutate(company_df, Emails_Clicked_Percent = Total_Emails_Clicked / Total_Emails_Delivered)
+company_df$Emails_Clicked_Percent <- ifelse(is.na(company_df$Emails_Clicked_Percent), 0, company_df$Emails_Clicked_Percent)
+company_df$Emails_Clicked_Percent
+
+
+
+na_count <- sapply(company_df, function(y) sum(length(which(is.na(y)))))
+na_count
+company_df <- filter(company_df, Total_Emails_Delivered >= 0)
+company_df$Total_Emails_Delivered
+
+company_df$Industry <- tolower(company_df$Industry)
+
+
 
 subdf <- company_df
 
-
-subdf$Is.Public <- NULL
+subdf$Industry <- as.factor(subdf$Industry)
 subdf$Create.Date <- NULL
 subdf$First.Contact.Create.Date <- NULL
 subdf$Close.Date <- NULL
@@ -279,7 +406,7 @@ subdf$Time.First.Seen    <- NULL
 subdf$Time.Last.Seen <- NULL
 subdf$Time.of.Last.Session <- NULL
 subdf$Time.of.First.Visit <- NULL
-subdf$Year.Founded <- NULL
+
 subdf$Website.URL <- NULL
 subdf$Facebook.Company.Page  <- NULL
 subdf$LinkedIn.Company.Page <- NULL
@@ -291,22 +418,48 @@ subdf$Street.Address <- NULL
 subdf$State.Region <- NULL
 subdf$City <- NULL
 subdf$Country <- NULL
-subdf$Industry <- NULL
-subdf$Number.of.Employees <- NULL
 subdf$Order_Five_Amount <- NULL
 subdf$Order_Four_Amount <- NULL
 subdf$Order_Three_Amount <- NULL
 subdf$Order_Two_Amount <- NULL
+subdf$Contact_Count <- NULL
+
 subdf$Order_One_Amount <- NULL
+subdf$Contact_One <- NULL
+subdf$Is.Public <- NULL
+subdf$Year.Founded <- NULL
 
 
+subdf$Industry <- NULL
+subdf$Number.of.Employees <- NULL
+
+subdf$Number.of.Visits <- NULL
+subdf$Total_Emails_Opened <- NULL
+subdf$Total_Emails_Clicked <- NULL
+subdf$Emails_Opened_Percent <- NULL
+subdf$Emails_Clicked_Percent <- NULL
+subdf$Associated.Contacts <- NULL
+subdf$Total_Emails_Delivered <- NULL
+subdf$First_Order_Traffic_Source <- NULL
+
+subdf1 <- filter(subdf, subdf$total_revenue > 2500)
+
+
+na_count <- sapply(subdf, function(y) sum(length(which(is.na(y)))))
+na_count
 
 str(subdf)
-subdf$Number.of.Pageviews
+head(subdf1, n = 30L)
 
-lm.1 <- lm(total_revenue~., data = subdf)
+
+# Total Revenue subdf
+subdf$total_order_count <- NULL
+lm.1 <- lm(total_revenue~. -Domain, data = subdf)
 summary(lm.1)
-str(lm.1)
+
+df.ans <- predict(lm.1, data=subdf)
+sqrt(mean((df.ans-subdf$total_revenue)^2))
+summary(subdf$total_revenue)
 
 
 # Algorythm
